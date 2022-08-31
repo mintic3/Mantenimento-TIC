c864732d6d89043eb7b581dcc8f4e4d688d0e859
Yanes3 <you@example.com>
2022-08-31 14:01:07 -0500

    Se nueva solucion y procesos de conexion

---
+++ Net/Mantenimento_TIC/Aplicacion.App/Aplicacion.App.Dominio/Clases/Persona.cs
@@ -0,0 +1,15 @@
+using System;
+namespace Aplicacion.App.Dominio
+{
+    public class Persona{
+        public int Id {get;set;}
+        public string Tid {get;set;}
+        public int Numero {get;set;}
+        public string Apellidos {get;set;}
+        public date FechaNato {get;set;}
+        public Genero Genero {get;set;}
+        public string Usuario {get;set;}
+        public string Contraseña {get;set;}
+ }
+
+}
