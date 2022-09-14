4a43a0c7150350085aefb011ecf1c2fd88f19d0e
Yanes3 <you@example.com>
2022-09-14 00:42:13 -0500

    se adelanta pagina de logueo

--- Net/Mantenimento_TIC/Aplicacion.App/Aplicacion.App.Frontend/Pages/User/usuarioRevision.cshtml.cs
+++ Net/Mantenimento_TIC/Aplicacion.App/Aplicacion.App.Frontend/Pages/User/usuarioRevision.cshtml.cs
@@ -4,11 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Aplicacion.App.Dominio;
+using Aplicacion.App.Persistencia;
 
 namespace MyApp.Namespace
 {
     public class usuarioRevisionModel : PageModel
     {
+        private readonly IRepositorioLogin _repo;
+        
+        public Login Login {get;set;}
+
+        public usuarioRevisionModel(IRepositorioLogin repositorio)
+        {
+            _repo = repositorio;
+        }
+
         public void OnGet()
         {
         }
